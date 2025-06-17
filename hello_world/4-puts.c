2a4e5c5cfe782f00ea07adcbc4a60390f4ee2335
chihaamin <amin.chiha@gmail.com>
2025-06-17 09:25:12 +0000

    Adding 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * prints a text followed by newline
- * Return: Always 0(Success)
+ * prints a text
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
