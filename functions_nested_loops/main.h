c7fc0248ab976e93d80ce11d57492809b309cf35
chihaamin <amin.chiha@gmail.com>
2025-06-17 14:52:02 +0100

    Adding 3-islower

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <unistd.h>
 
@@ -6,3 +9,7 @@ int _putchar(char c);
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
+
+int _islower(int c);
+
+#endif /* MAIN_H */
