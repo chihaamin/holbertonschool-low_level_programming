019f77d46b314422469bcec7fa39249c8dbee75a
chihaamin <amin.chiha@gmail.com>
2025-06-18 14:47:25 +0100

    Adding 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,15 +10,19 @@
  */
 void puts2(char *str)
 {
+	int i;
+
 	if (str == NULL)
 	{
 		return;
 	}
 
-	while (*str != '\0')
+	for (i = 0; str[i] != '\0'; i + 2)
 	{
-		_putchar(*str);
-		str++;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 
 	_putchar('\n');
