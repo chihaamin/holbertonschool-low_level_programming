6718aea7cc47bcfd0df917a5ea295be8b491492c
chihaamin <amin.chiha@gmail.com>
2025-06-18 10:34:14 +0100

    Adding 5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = '0'; i <= '14'; i++)
+		for (i = '0'; i < '15'; i++)
 		{
 			_putchar(i);
 		}
-		}
+	}
 
 	_putchar('\n');
 }
