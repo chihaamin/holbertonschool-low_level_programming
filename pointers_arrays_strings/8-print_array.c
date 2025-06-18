c569942f3e93af45d63a59ecf0600e0a618762f9
chihaamin <amin.chiha@gmail.com>
2025-06-18 14:08:43 +0100

    Adding 8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: the array to print
+ * @n: the number of elements to print
+ *
+ * Description: This function prints the first n elements of the array
+ * pointed to by a, followed by a new line. If a is NULL or n is less than or
+ * equal to 0, it does nothing.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (a == NULL || n <= 0)
+	{
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
