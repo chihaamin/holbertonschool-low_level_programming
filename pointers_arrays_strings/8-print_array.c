dbbdc5bdcc011c8ead6aa8dfe23b699bf00fa334
chihaamin <amin.chiha@gmail.com>
2025-06-18 14:34:32 +0100

    Adding 8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -17,13 +17,14 @@ void print_array(int *a, int n)
 	{
 		return;
 	}
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (i > 0)
-		{
-			printf(", ");
-		}
 		printf("%d", a[i]);
+
+		if (i == n - 1)
+			continue;
+
+		printf(", ");
 	}
 
 	printf("\n");
