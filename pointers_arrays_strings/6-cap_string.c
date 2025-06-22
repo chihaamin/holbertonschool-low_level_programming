510e9f0b43062f976d241da8748e7ccdab0e802e
chihaamin <amin.chiha@gmail.com>
2025-06-22 20:35:10 +0100

    Adding 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,31 +6,24 @@
  */
 char *cap_string(char *s)
 {
-	int index = 0;
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (s[index])
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		while (!(s[index] >= 'a' && s[index] <= 'z'))
-			index++;
-
-		if (s[index - 1] == ' ' ||
-			s[index - 1] == '\t' ||
-			s[index - 1] == '\n' ||
-			s[index - 1] == ',' ||
-			s[index - 1] == ';' ||
-			s[index - 1] == '.' ||
-			s[index - 1] == '!' ||
-			s[index - 1] == '?' ||
-			s[index - 1] == '"' ||
-			s[index - 1] == '(' ||
-			s[index - 1] == ')' ||
-			s[index - 1] == '{' ||
-			s[index - 1] == '}' ||
-			index == 0)
-			s[index] -= 32;
-
-		index++;
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
 	}
-
 	return (s);
 }
