234e0abe09009bdef86e6366ecec514f78023990
chihaamin <amin.chiha@gmail.com>
2025-06-22 20:29:48 +0100

    Adding 6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * *cap_string - Capitalizes all words of a string
+ * @s: The string to capitalize
+ * * Return: Pointer to the resulting string s
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	char separators[] = " \t\n,;.!?\"(){}";
+	int is_separator = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (is_separator && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+			is_separator = 0;
+		}
+		else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+				 s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+				 s[i] == '!' || s[i] == '?' || s[i] == '"' ||
+				 s[i] == '(' || s[i] == ')' || s[i] == '{' ||
+				 s[i] == '}')
+		{
+			is_separator = 1;
+		}
+		else
+		{
+			is_separator = 0;
+		}
+	}
+	return (s);
+}
