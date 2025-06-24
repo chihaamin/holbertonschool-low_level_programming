019664dd03888bb8573665871d6b6a70e8c1e992
chihaamin <amin.chiha@gmail.com>
2025-06-24 10:22:54 +0100

    Adding 8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,14 +9,21 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int sum1 = 0, sum2 = 0;
+	int index, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < size; i++)
+	for (index = 0; index < size; index++)
 	{
-		sum1 += a[i * size + i];
-		sum2 += a[i * size + (size - 1 - i)];
-
-		printf("%d, %d\n", sum1, sum2);
+		sum1 += a[index];
+		a += size;
 	}
+
+	a -= size;
+
+	for (index = 0; index < size; index++)
+	{
+		sum2 += a[index];
+		a -= size;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
