f71dfc366ee555c0a291f69ab62d03d3d4f61acb
chihaamin <amin.chiha@gmail.com>
2025-06-27 12:32:57 +0100

    Add 0-whatsmyname.c to print the program name from command line arguments

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point.
+ * @argc: The number of command line arguments.
+ * @argv: The program command line arguments.
+ * Description: A program that prints the name of the program>
+ * Return: Always Success (0).
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
