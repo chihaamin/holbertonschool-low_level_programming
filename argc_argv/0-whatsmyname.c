629ab343e9d53801e749cb4fa204f2a94ceb539a
chihaamin <amin.chiha@gmail.com>
2025-06-27 12:33:50 +0100

    Fix main function signature to remove unused argc parameter

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  * main - Entry point.
- * @argc: The number of command line arguments.
  * @argv: The program command line arguments.
  * Description: A program that prints the name of the program>
  * Return: Always Success (0).
  */
 
-int main(int argc, char *argv[])
+int main(char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
