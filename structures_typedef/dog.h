9e39f3903b218b7c699b5b017acfc7126b60f38f
chihaamin <amin.chiha@gmail.com>
2025-07-01 12:17:30 +0100

    Add dog structure definition and header file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog struct
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ *
+ * Description: A dog basic informations
+ */
+
+struct dog
+{
+	float age;
+
+	char *name;
+
+	char *owner;
+};
+
+#endif /* DOG_H */
