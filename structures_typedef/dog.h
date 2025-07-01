aacc8885cde6cf03817518d1a702e898f8d23217
chihaamin <amin.chiha@gmail.com>
2025-07-01 13:33:24 +0100

    Fix formatting in dog.h: align struct members for improved readability

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,11 +16,11 @@
 
 typedef struct dog
 {
-	float age;
+    float age;
 
-	char *name;
+    char *name;
 
-	char *owner;
+    char *owner;
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -28,5 +28,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
