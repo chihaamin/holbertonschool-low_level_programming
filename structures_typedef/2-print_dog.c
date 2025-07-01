6748e447139f3c511bf694a1683b8e2b48b07484
chihaamin <amin.chiha@gmail.com>
2025-07-01 12:56:29 +0100

    Refactor print_dog function for clarity and consistency in output formatting

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -7,10 +7,22 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		(d->name && printf("Name: %s\n", d->name)) || printf("Name: (nil)\n");
-
-		(d->age && printf("Age: %.6f\n", d->age)) || printf("Age: (nil)\n");
-
-		(d->owner && printf("Owner: %s\n", d->owner)) || printf("Owner: (nil)\n");
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+		printf("Age: %.2f\n", d->age);
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
 	}
 }
