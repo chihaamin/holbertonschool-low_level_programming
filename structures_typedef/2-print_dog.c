401c670e8392b27145794884a33475851c19b139
chihaamin <amin.chiha@gmail.com>
2025-07-01 12:46:05 +0100

    Add print_dog function to display dog information

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * print_dog - Prints a dog informations
+ * @d: Dog struct instance
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		(d->name && printf("Name: %s\n", d->name)) || printf("Name: (nil)\n");
+
+		(d->age && printf("Age: %.6f\n", d->age) || printf("Age: (nil)\n"));
+
+		(d->owner && printf("Owner: %s\n", d->owner)) || printf("Owner: (nil)\n");
+	}
+}
