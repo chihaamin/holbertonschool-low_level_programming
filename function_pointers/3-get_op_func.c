835fbc77a0e5331dc351352e0a04450e90c28191
chihaamin <amin.chiha@gmail.com>
2025-07-10 11:02:54 +0100

    Refactor get_op_func to simplify operator selection and input validation

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -16,32 +16,14 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}};
-	int index;
+	int i;
 
-	/* Validate input: NULL, empty string, or multi-character operator */
-	if (s == NULL || s[0] == '\0' || s[1] != '\0')
-		return (NULL);
-
-	switch (s[0])
+	i = 0;
+	while (ops[i].op != NULL)
 	{
-	case '+':
-		index = 0;
-		break;
-	case '-':
-		index = 1;
-		break;
-	case '*':
-		index = 2;
-		break;
-	case '/':
-		index = 3;
-		break;
-	case '%':
-		index = 4;
-		break;
-	default:
-		return (NULL);
+		if (*(ops[i].op) == *s && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
 	}
-
-	return (ops[index].f);
+	return (NULL);
 }
