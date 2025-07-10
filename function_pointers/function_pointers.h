48ee3cfa41927f2600443d6163f4c9eb17c97341
chihaamin <amin.chiha@gmail.com>
2025-07-10 10:35:06 +0100

    Add _putchar function implementation and update function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,6 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Function prototypes*/
+
+int _putchar(char c);
+
+void print_name_as_is(char *name);
+
+void print_name_uppercase(char *name);
+
+void print_elem(int elem);
+
+void print_elem_hex(int elem);
+
+int is_98(int elem);
+
+int is_strictly_positive(int elem);
+
+int abs_is_98(int elem);
+
+/* Function prototypes for function pointers exercies*/
+
 void print_name(char *name, void (*f)(char *));
 
 void array_iterator(int *array, size_t size, void (*action)(int));
