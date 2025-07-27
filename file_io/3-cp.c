f6893b02b20a4f0be82c857e37678c25b92880db
chihaamin <amin.chiha@gmail.com>
2025-07-27 11:40:30 +0100

    fix cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close_file(fd_from);
