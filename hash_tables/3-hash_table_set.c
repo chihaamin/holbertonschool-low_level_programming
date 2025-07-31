0706989363e23fe870a2adf7db09074b66587c2b
chihaamin <amin.chiha@gmail.com>
2025-07-31 15:58:00 +0100

    adding helper function to hash_table_set to match betty style

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,39 @@
 #include "hash_tables.h"
 
 /**
+ * create_node - Creates a new hash node.
+ * @key: The key for the new node.
+ * @value: The value for the new node.
+ *
+ * Return: A pointer to the newly created node, or NULL on failure.
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
  * hash_table_set - Adds an element to the hash table.
  * @ht: The hash table to add or update the key/value pair.
  * @key: The key, which cannot be an empty string.
@@ -32,25 +65,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	/* Create a new node */
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = create_node(key, value);
 	if (new_node == NULL)
 		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
